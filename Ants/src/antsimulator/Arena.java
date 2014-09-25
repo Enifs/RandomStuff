1f6fb2b2d2c230b221f64e3d568c946c4fab7cc9
Enifs <d.romanovskis@gmail.com>
2014-09-26 01:02:42 +0300

    Arena defined.

---
+++ Ants/src/antsimulator/Arena.java
@@ -0,0 +1,42 @@
+/**
+ * Created by Enifs.
+ */
+
+package antsimulator;
+
+import java.awt.*;
+
+
+/**
+ * Arena represents rectangle in ++ quadrant. with one corner in (0,0).
+ * Width and Height represents number of rows and collums respectively.
+ */
+public class Arena
+{
+	// ----------------------------------------------------------------------------
+	// Section: Constructors
+	// ----------------------------------------------------------------------------
+
+	public Arena(int width, int heigth)
+	{
+		this.width = width;
+		this.heigth = heigth;
+	}
+
+	// ----------------------------------------------------------------------------
+	// Section: Topology methods
+	// ----------------------------------------------------------------------------
+
+	public boolean contains(Point point)
+	{
+		return point.getX() > 0 && point.getX() <= this.width &&
+				point.getY() > 0 && point.getY() <= this.heigth;
+	}
+
+	// ----------------------------------------------------------------------------
+	// Section: Fields
+	// ----------------------------------------------------------------------------
+
+	int width = 0;
+	int heigth = 0;
+}
